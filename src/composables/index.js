288cc54cf219c807e69d568b67a881dba4170b7d
skywalkershen <skywalkershen@hotmail.com>
2023-05-17 18:17:42 +0800

    Modify lava shaders

--- src/composables/index.js
+++ src/composables/index.js
@@ -13,7 +13,10 @@ const scene = new THREE.Scene(),
 
 let controls,
     renderer,
-    lavaUniforms;
+    lavaUniforms,
+    lavaSurfaceUniforms,
+    count = 0,
+    surfaceGeometry;
 
 loadModel();    
 export function init (containerRef) {
@@ -45,13 +48,45 @@ function loadModel () {
         let model = gltf.scene;
         scene.add( gltf.scene );
         let lavaBody = model.getObjectByName('Sphere008_Wall_Paint_0'),
-            lavaTube = model.getObjectByName('LavaTube');
-        loadAndApplyLavaSurfaceShader([lavaBody, lavaTube]);
+            lavaTube = model.getObjectByName('LavaTube'),
+            lavaSurface = model.getObjectByName('LavaSurface')
+        loadAndApplyLavaShader([lavaBody, lavaTube, lavaSurface]);
+        // loadAndApplyLavaSurfaceShader(lavaSurface);
 
+       
         window.devModel = gltf.scene
     } );
 }
-async function loadAndApplyLavaSurfaceShader (meshes=[]) {
+async function loadAndApplyLavaSurfaceShader (mesh) {
+    lavaSurfaceUniforms = {
+        u_pointsize: { value: 2.0 },
+        // wave 1
+        u_noise_freq_1: { value: 3.0 },
+        u_noise_amp_1: { value: 0.2 },
+        u_spd_modifier_1: { value: 1.0 },
+        // wave 2
+        u_noise_freq_2: { value: 2.0 },
+        u_noise_amp_2: { value: 0.3 },
+        u_spd_modifier_2: { value: 0.8 },
+        texture1: { value: textureLoader.load( 'noise_file.png' ) },
+        texture2: { value: textureLoader.load( 'lava_file.jpg' ) }
+    };
+    lavaSurfaceUniforms[ 'texture1' ].value.wrapS = lavaSurfaceUniforms[ 'texture1' ].value.wrapT = THREE.RepeatWrapping;
+    lavaSurfaceUniforms[ 'texture2' ].value.wrapS = lavaSurfaceUniforms[ 'texture2' ].value.wrapT = THREE.RepeatWrapping;
+
+    const material = new THREE.ShaderMaterial( {
+
+        uniforms: lavaSurfaceUniforms,
+        vertexShader: lavaShader.vertSurface,
+        fragmentShader: lavaShader.frag
+
+    } );
+
+    mesh.material = material;
+
+    window.devlavaSurfaceUniforms = lavaSurfaceUniforms;
+}
+async function loadAndApplyLavaShader (meshes=[]) {
     lavaUniforms = {
         'fogDensity': { value: 0.45 },
         'fogColor': { value: new THREE.Vector3( 0, 0, 0 ) },
@@ -87,7 +122,13 @@ function animate() {
 
 	if (lavaUniforms) lavaUniforms[ 'time' ].value += 0.2 * delta;
     
-
+    surfaceGeometry?.vertices?.forEach?.((vert, i) => {
+        let z = +vert.z;
+        vert.z = Math.sin(( i + count * 0.00002)) * (vert._myZ - (vert._myZ* 0.6))
+        surfaceGeometry.verticesNeedUpdate = true;
+  
+        count += 0.1
+    })
     controls.update();
     renderer.render( scene, camera );
 
--- src/composables/lavaShader.js
+++ src/composables/lavaShader.js
@@ -36,7 +36,7 @@ export const frag = `
         float depth = gl_FragCoord.z / gl_FragCoord.w;
         const float LOG2 = 1.442695;
         float fogFactor = exp2( - fogDensity * fogDensity * depth * depth * LOG2 );
-        fogFactor = 1.0 - clamp( fogFactor, 0.0, 1.0 );
+        fogFactor = 0.5 - clamp( fogFactor, 0.0, 0.5 );
 
         gl_FragColor = mix( gl_FragColor, vec4( fogColor, gl_FragColor.w ), fogFactor );
 
